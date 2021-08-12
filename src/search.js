394858ca0f20bc1a5286415968d640d579456446
Minh Reigen <mreigen@gmail.com>
2021-08-12 10:10:37 -0700

    fixed indentations - 2 spaces instead of tabs

--- src/search.js
+++ src/search.js
@@ -3,37 +3,39 @@ import React from "react";
 import axios from "axios";
 
 import "./css/song.css";
-export default class SongSearch extends React.Component{
-	constructor(){
-		super()
-	}
-	search(){
+export default class SongSearch extends React.Component {
+  constructor() {
+    super();
+  }
 
-		var options = {
-		  method: 'GET',
-		  url: 'https://shazam-core.p.rapidapi.com/v1/tracks/search',
-		  params: {query: 'Wellerman'},
-		  headers: {
-			'x-rapidapi-key': '',
-			'x-rapidapi-host': 'shazam-core.p.rapidapi.com'
-		  }
-		};
+  search() {
+    var options = {
+      method: "GET",
+      url: "https://shazam-core.p.rapidapi.com/v1/tracks/search",
+      params: { query: "Wellerman" },
+      headers: {
+        "x-rapidapi-key": "",
+        "x-rapidapi-host": "shazam-core.p.rapidapi.com",
+      },
+    };
 
-		axios.request(options).then(function (response) {
-			console.log(response.data);
-		}).catch(function (error) {
-			console.error(error);
-		});
-		console.log("search in progress")
-	}
-	render(){
-			
-		return (
-			<>
-				<input type="text" />
-				<button onClick={this.search}>search</button>
-			</>
-		);
-	}
-	
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    console.log("search in progress");
+  }
+
+  render() {
+    return (
+      <>
+        <input type="text" />
+        <button onClick={this.search}>search</button>
+      </>
+    );
+  }
 }
